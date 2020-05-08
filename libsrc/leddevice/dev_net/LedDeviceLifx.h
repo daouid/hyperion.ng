6678d7f17663e606fba6a6b58b88639339218c2f
daouid <daouid@gmail.com>
2020-05-09 01:30:34 +0200

    more research on prior art

--- libsrc/leddevice/dev_net/LedDeviceLifx.h
+++ libsrc/leddevice/dev_net/LedDeviceLifx.h
@@ -30,6 +30,10 @@ const ushort LIFX_DEFAULT_PORT = 56700;
 // lifx device discovery:
 /// The protocol documentation includes a section on workflows including how to do discovery. Essentially it works like this: Send a Device::GetService packet to the broadcast address (255.255.255.255) on port 56700
 /// Each bulb will respond with a list of service they support as a Device::StateService 23 message. The only service currently documented is UDP which has its type field set to 1. When your application receives one of these messages it should store the sender address and the service port internally. This will constitute the list of LIFX devices that you have found. source: https://community.lifx.com/t/discovering-lifx-bulbs/265
+///The basic premise is you create a UDP socket and then throw bytes at it. It’s recommended you only broadcast the GetService 2 to 255.255.255.255 and use the StateService replies to know what devices are on your network.
+
+/// Then you “unicast” bytes to specific devices, which is the same as a broadcast, but instead of sending to 255.255.255.255 you send to the specific IP of the device you want to talk to. https://community.lifx.com/t/how-to-send-a-packet-to-a-lifx-bulb-using-the-lifx-lan-protocol/6502
+
 
 static const char LIFX_DEFAULT_HOST[] = "255.255.255.255";
 
